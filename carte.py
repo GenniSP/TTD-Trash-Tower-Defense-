33613f8115196b5d6ff9ff15e2210a8717871b7d
GenniSP <66nr5pmmtj@privaterelay.appleid.com>
2026-06-24 11:17:27 +0200

    aggiunta carte.py

---
+++ carte.py
@@ -0,0 +1,30 @@
+import random as rnd
+
+
+class Carta:
+    def __init__(self,power,tipo,costo,vita):
+        self.costo=costo
+        self.power=power
+        self.tipo=tipo
+        self.vita=vita
+        
+
+    
+
+
+
+def spawn_carte():
+    tipo=rnd.randint(0,2)
+    power=-1
+    vita=-1
+    if tipo==0:
+        power=rnd.randint(1,3)
+        vita=rnd.randint(5,6)
+    if tipo==1:
+        power=rnd.randint(4,7)
+        vita=rnd.randint(6,8)
+    if tipo==2:
+        power=rnd.randint(8,10)
+        vita=rnd.randint(8,10)
+
+    
