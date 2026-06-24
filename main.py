d828fc14e4f715869701c827dbfc5495c4077e4b
GenniSP <66nr5pmmtj@privaterelay.appleid.com>
2026-06-24 12:15:43 +0200

    fixed display flip and implemented balance of 3,5 sec +=2 mana

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ background = pygame.image.load('background1.png')
 background = pygame.transform.scale(background, (sizeX, sizeY))
 myScreen.blit(background, (0, 0))
 
-game_font=pygame.font.Font(None,36)
+game_font=pygame.font.Font(None,45)
 
 game=True
 mana=0
@@ -23,11 +23,14 @@ ck=pygame.time.Clock()
 passed=0
 while game:
     passed+=ck.tick(60) #imposto 60 fps e ottengo in passed quanto tempo è passato dall'ultimo frame
-    if passed>=1200:
+    if passed>=3500:
         mana+=2
-        passed-=1200 #tolgo il tempo trascorso/non resetto a 0 altrimenti perderei probabiblmente qualche millisecondo
+        passed-=3500 #tolgo il tempo trascorso/non resetto a 0 altrimenti perderei probabiblmente qualche millisecondo
     mana_text=game_font.render(f"Mana: {mana}",True, (255,255,255))
-    myScreen.blit(mana_text,(80,350))
+
+    myScreen.blit(mana_text,(315,25))
+    pygame.display.flip() #ricarica con il mana
+    myScreen.blit(background, (0, 0)) #fai ritornare il background
 
 
 
