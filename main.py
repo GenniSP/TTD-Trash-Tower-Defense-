e98337f42074f5edd089d28e1bef377f64d310e2
djstone10 <pietro.ballestri@gmail.com>
2026-06-24 11:26:31 +0200

    impostazione base schermo + background

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import pygame
 
+# initialize the environment
+pygame.init()
+
+# define the screen size
+sizeX = 225
+sizeY = 400
+myScreen= pygame.display.set_mode((sizeX, sizeY))
+pygame.display.set_caption('DTT')
+
+# custom background
+background = pygame.image.load('background1.png') 
+# fit to window size
+background = pygame.transform.scale(background, (sizeX, sizeY))
+myScreen.blit(background, (0, 0))
+
+while True:
+    pygame.display.flip()
+
+x=0
