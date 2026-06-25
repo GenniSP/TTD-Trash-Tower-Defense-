df76e674a152362c77b9f0a7e64c4f2e6c58992c
primeranodiego3-eng <primeranodiego3@gmail.com>
2026-06-25 16:20:45 +0200

    musica quasi finita

--- musica.py
+++ musica.py
@@ -1,37 +1,27 @@
 import pygame
-import time
 
+
+pygame.init()
 pygame.mixer.init()
 
+playlist = ["song_1.ogg", "song_2.mp3", "song_3.mp3"]
+canzone_suonata= 0
 
-playlist = ["peppe1.mp3", "Sayonara Fujisan.mp3", "Tokyo Reggie.mp3"]
-indice_canzone = 0
-
-# Fai partire la prima canzone
-pygame.mixer.music.load(f"musica/{playlist[indice_canzone]}")
+pygame.mixer.music.load(playlist[canzone_suonata])
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play()
 
+clock = pygame.time.Clock()
 
-gioco_in_corso = True
-while gioco_in_corso:
-    
-    
+while True:
     if not pygame.mixer.music.get_busy():
-        
-        indice_canzone = indice_canzone + 1
-        
-        
-        if indice_canzone >= len(playlist):
-            indice_canzone = 0
-            
-        
-        pygame.mixer.music.load(playlist[indice_canzone])
+        canzone_suonata = canzone_suonata + 1
+        if canzone_suonata >= len(playlist):
+            canzone_suonata = 0
+        pygame.mixer.music.load(playlist[canzone_suonata])
         pygame.mixer.music.play()
-        print(f"Nuova canzone partita: {playlist[indice_canzone]}")
-
-    
-    time.sleep(1) 
-
-
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    clock.tick(10)
