c582aa6e9717743bcb7b3ffad2dcc7c929583da0
GenniSP <66nr5pmmtj@privaterelay.appleid.com>
2026-06-25 14:16:44 +0200

    implemented the dynamic pricing for the cards based on their type (not on strength or health)

--- carte.py
+++ carte.py
@@ -17,16 +17,20 @@ def spawn_carte():
     tipo=rnd.randint(0,2)
     power=-1
     vita=-1
+    costo=-1
     if tipo==0:
         power=rnd.randint(1,3)
         vita=rnd.randint(5,6)
+        costo=rnd.randint(1,3)
     if tipo==1:
         power=rnd.randint(4,7)
         vita=rnd.randint(6,8)
+        costo=rnd.randint(4,5)
     if tipo==2:
         power=rnd.randint(8,9)
         vita=rnd.randint(8,9)
-    return Carta(power,tipo,rnd.randint(3,8),vita)
+        costo=rnd.randint(6,7)
+    return Carta(power,tipo,costo,vita)
 
 def make_list_cards():
     card1=spawn_carte()
